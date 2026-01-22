fff45aac07a189d44bfb52f91c4cb9cabd1b3889
Alessandro Casagrande <casagrande.alessandro.03@gmail.com>
2026-01-22 15:39:49 +0100

    Add cleanup also before running topology

--- run_net.sh
+++ run_net.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+# Cleanup
+echo "Cleaning up old containers and DNS records..."
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
+
+# Remove old DNS records
+rm -rf config/dns_config/zones/*
+
 # Run topology
+echo "Running topology..."
 if ! command -v python3.9 >/dev/null 2>&1
 then
     sudo python3 ./topology.py
@@ -9,7 +18,7 @@ else
 fi
 
 # Cleanup
-echo "Cleaning up..."
+echo "Execution finished, cleaning up..."
 sudo mn -c
 docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
